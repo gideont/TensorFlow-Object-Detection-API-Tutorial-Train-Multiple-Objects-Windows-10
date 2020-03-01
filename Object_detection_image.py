7fbf84cc7a14649fb220f94bcb147223c13f9295
Gideon Tsang <gideont@gmail.com>
2020-03-02 01:12:50 +0800

    Added coco labelmap and corrected to 90 classes

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -31,8 +31,7 @@ from utils import visualization_utils as vis_util
 
 # Name of the directory containing the object detection module we're using
 MODEL_NAME = 'inference_graph'
-IMAGE_NAME = 'test2.jpg'
-#IMAGE_NAME = 'test1.JPG'
+IMAGE_NAME = 'test1.jpg'
 
 # Grab path to current working directory
 CWD_PATH = os.getcwd()
@@ -48,7 +47,7 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
 PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_NAME)
 
 # Number of classes the object detector can identify
-NUM_CLASSES = 6
+NUM_CLASSES = 90
 
 # Load the label map.
 # Label maps map indices to category names, so that when our convolution
